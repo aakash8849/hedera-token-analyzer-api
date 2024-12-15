e822322e2a4afc549efdc80ec8415adc4b186aaf
aakash8849 <aakash000bpatel@gmail.com>
2024-12-15 22:24:53 +0530

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,6 +3,7 @@ import { analyzeToken, visualizeToken } from '../controllers/tokenController.js'
 
 const router = express.Router();
 
+// These routes will be mounted under /api
 router.post('/analyze', analyzeToken);
 router.get('/visualize/:tokenId', visualizeToken);
 
