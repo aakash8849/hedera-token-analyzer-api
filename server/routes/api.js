c88cc5f3ffcd9668bfda7f98527b8bbdd855a579
aakash8849 <aakash000bpatel@gmail.com>
2024-12-18 13:19:57 +0530

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,17 @@
 import express from 'express';
-import { analyzeToken, getAnalysisStatus, visualizeToken } from '../controllers/tokenController.js';
+import { 
+    analyzeToken, 
+    getAnalysisStatus, 
+    getOngoingAnalyses,
+    visualizeToken 
+} from '../controllers/tokenController.js';
 
 const router = express.Router();
 
 // Analysis endpoints
 router.post('/analyze', analyzeToken);
 router.get('/analyze/:tokenId/status', getAnalysisStatus);
+router.get('/analyze/ongoing', getOngoingAnalyses);
 router.get('/visualize/:tokenId', visualizeToken);
 
 export default router;
