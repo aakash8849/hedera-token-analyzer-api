aefa0ef9400db845d31d7b06e0ed3b1322c747d3
aakash8849 <aakash000bpatel@gmail.com>
2025-01-16 11:25:30 +0530

    Create Transaction.js

---
+++ server/models/Transaction.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const transactionSchema = new mongoose.Schema({
+  tokenId: {
+    type: String,
+    required: true,
+    index: true
+  },
+  timestamp: {
+    type: Date,
+    required: true,
+    index: true
+  },
+  sender: {
+    type: String,
+    required: true
+  },
+  receiver: {
+    type: String,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  involvesTreasury: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true });
+
+// Compound index for faster queries
+transactionSchema.index({ tokenId: 1, timestamp: -1 });
+
+export const Transaction = mongoose.model('Transaction', transactionSchema);
