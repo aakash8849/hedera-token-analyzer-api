e3ad3b97cd51cff80f33637cd7b463b97e73f45d
aakash8849 <aakash000bpatel@gmail.com>
2025-01-16 11:24:04 +0530

    Create Holder.js

---
+++ server/models/Holder.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const holderSchema = new mongoose.Schema({
+  tokenId: {
+    type: String,
+    required: true,
+    index: true
+  },
+  account: {
+    type: String,
+    required: true
+  },
+  balance: {
+    type: Number,
+    required: true
+  },
+  isTreasury: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true });
+
+// Compound index for faster queries
+holderSchema.index({ tokenId: 1, account: 1 }, { unique: true });
+
+export const Holder = mongoose.model('Holder', holderSchema);
